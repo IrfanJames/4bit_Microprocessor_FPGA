0463ac957d80cf44942e913614ef290a1d46c9c9
IrfanJames <irfanjamespak@gmail.com>
2023-03-05 22:30:10 +0500

    Complete CPU (Number_Cruncher); Good RTL_Viwer

--- DE1_SOC/All_Modules/Number_Cruncher.v
+++ DE1_SOC/All_Modules/Number_Cruncher.v
@@ -1,34 +1,39 @@
-module Number_Cruncher(clk, S, S_reg, D1, D0, J, in, Op_Code, Cout, Display1, Display0, Display3, Display2);
-	input clk, S, S_reg, D1, D0, J;
-	input  [3:0] in;
+module Number_Cruncher(clk, Op_Code_Leds, Display1, Display0, Display3, Display2);
+	input clk;
 	output [6:0] Display1, Display0, Display3, Display2;
-	output Cout;
+	output [7:0] Op_Code_Leds;
 	
-	wire   [3:0] PC_Out;
-	Program_Counter PC (clk, J, in, PC_Out);
-	
-	output [7:0] Op_Code;
-	RAM RAM_A (PC_Out, Op_Code);
-	
+	wire         ALU_Cout;
 	wire   [3:0] ALU_InA;
 	wire   [3:0] ALU_InB;
 	wire   [3:0] ALU_Out;
 	
+	wire   [3:0] PC_Out;
+	wire J, C, D1, D0, S_reg, S_C2, C1, C0; // {...} is OP_Code
+	
+	wire DFF_Out_Cout, jump;
+	D_ff DF_A (clk, ALU_Cout, DFF_Out_Cout);
+	assign jump = J | (C & DFF_Out_Cout);
+	Program_Counter PC (clk, jump, {1'b0, S_C2, C1, C0}, PC_Out); // {...} is part of OP_Code
+	
+	RAM RAM_A (PC_Out, {J, C, D1, D0, S_reg, S_C2, C1, C0}); // {...} is OP_Code
+	assign Op_Code_Leds = {J, C, D1, D0, S_reg, S_C2, C1, C0}; // {...} is OP_Code
+	
+	
 	wire   [3:0] MUX_Out;
-	mux4bit_2to1 Mux_A (ALU_Out, in, S_reg, MUX_Out);
+	mux4bit_2to1 Mux_A (ALU_Out, {1'b0, S_C2, C1, C0}, S_reg, MUX_Out); // {...} is part of OP_Code
 	
 	wire   [2:0] Reg_En;
 	wire   [3:0] Reg_O_Out;
 	Register_En_Decoder Reg_En_A (D1, D0, Reg_En);
 	
-	Register_4bit Reg_A (MUX_Out, Reg_En[0], 1, clk, ALU_InA);
-	Register_4bit Reg_B (MUX_Out, Reg_En[1], 1, clk, ALU_InB);
-	Register_4bit Reg_O (ALU_InA, Reg_En[2], 1, clk, Reg_O_Out);
+	Register_4bit Reg_A (MUX_Out, Reg_En[0], 1'b1, clk, ALU_InA);
+	Register_4bit Reg_B (MUX_Out, Reg_En[1], 1'b1, clk, ALU_InB);
+	Register_4bit Reg_O (ALU_InA, Reg_En[2], 1'b1, clk, Reg_O_Out);
 	
-	Adder_n ALU (S, ALU_InA, ALU_InB, ALU_Out, Cout);
+	Adder_n ALU (S_C2, ALU_InA, ALU_InB, ALU_Out, ALU_Cout);
 	
 	Display_7_Segment Display_A (Reg_O_Out, Display1, Display0);
-	
 	Display_7_Segment Display_B (PC_Out, Display3, Display2);
 	
 endmodule
---
+++ DE1_SOC/All_Modules/test_Cruncher.v
@@ -0,0 +1,46 @@
+module test_Cruncher();
+	reg clk;
+	wire Op_Code_Leds;
+	
+	initial
+	begin
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+		clk = 0; #10;	clk = 1; #10;
+	end
+	
+endmodule
